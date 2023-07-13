913e35ef4d8379cb77d96df441b2554efa689dcb
Felipe Muñoz <analista.felipe.m@gmail.com>
2023-07-13 01:46:45 -0400

    Se crea la estructura html de Home.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<template>
+    <nav class="navbar-menu">
+        <!-- <router-link to="/">Home</router-link> -->
+        <h3><i class="bi bi-controller"></i>Games Opinion</h3>
+        <router-link to="/" class="navbar-link"><i class="bi bi-house"></i>Home</router-link>
+    </nav>
+    <!-- <nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Games Opinion</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link to="/">Home</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav> -->
+</template>
+
+<script>
+export default {
+    name: 'nav-bar-comp',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    // methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    .navbar-menu {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        padding: 8px 16px;
+        color: white;
+        background-color: black;
+    }
+    .navbar-menu h3 {
+        font-size: 20px;
+    }
+    .navbar-menu i {
+        margin-right: 5px;
+    }
+    .navbar-link {
+        color: white;
+        text-decoration: none;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Lista de juegos disponibles</h1>
+    <div class="cards-game">
+      <div class="card" style="width: 18rem;">
+        <img src="https://picsum.photos/id/684/600/400" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">NOMBRE</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">RATING</li>
+          <li class="list-group-item">RELEASED</li>
+          <li class="list-group-item">UPDATE</li>
+        </ul>
+        <div class="card-body">
+          <button class="btn btn-primary">Opinar</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+
+<style scoped>
+  .cards-game {
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+</style>
